67e99fbd6eb3fbd7385abfa6d9a731591e748e2d
nickthelegend <testingtesla7@gmail.com>
2023-08-14 08:22:56 +0400

    Classes

---
+++ 1.Classes/classes.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+class YoutubeChannel{
+public:
+    string name;
+    string ownerName;
+    int subscribersCount;
+    list<string> PublishedVideoTitles;
+
+
+
+
+
+
+
+
+};
+
+
+
+
+int main(){
+YoutubeChannel ytchannel;
+
+ytchannel.name = "SAD";
+cout<< ytchannel.name<<endl;
+
+
+}
